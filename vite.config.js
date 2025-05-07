3023986978be77f27da8529effe2d99ec50ea0d2
Haitham Desouky <haithamadesouky@gmail.com>
2025-05-07 17:18:27 +0200

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/css-to-jss-converter/", // Update this to your GitHub repository name
   plugins: [react()],
   resolve: {
     alias: {
